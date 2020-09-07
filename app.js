0d673e5f961d73e2955290d07991cfb671ccaf16
Panu Valtanen <p4nu@users.noreply.github.com>
2020-09-07 13:49:54 +0300

    4.2: blogilista step2 - luotu app.js ja siirretty express-koodi sinne.

---
+++ app.js
@@ -0,0 +1,35 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const cors = require('cors');
+const Blog = require('./models/blog');
+
+const mongoUrl = process.env.MONGODB_URI;
+mongoose.connect(mongoUrl, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/api/blogs', (request, response) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs);
+    });
+});
+
+app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body);
+
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result);
+    });
+});
+
+module.exports = app;
