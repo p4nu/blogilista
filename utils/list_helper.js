3fccfa30d06a29813e0beeab0908b09f78f9afc9
Panu Valtanen <p4nu@users.noreply.github.com>
2020-09-08 08:14:38 +0300

    4.5: apufunktioita ja yksikkötestejä, step3 välicommit: luo uusi testi

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -6,7 +6,12 @@ const totalLikes = blogs => {
   return blogs.reduce((sum, blog) => sum + blog.likes, 0);
 };
 
+const favoriteBlog = blogs => {
+
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
