aa4999473e06374f8de5e4f05e17c069c3ed9c3a
Panu Valtanen <p4nu@users.noreply.github.com>
2020-09-22 10:23:11 +0300

    4.8: blogilistan testit, step1 tehty

---
+++ tests/blog_list_api.test.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+const supertest = require('supertest');
+const app = require('../app');
+const api = supertest(app);
+
+const Blog = require('../models/blog');
+
+const initialBlogs = [{
+  title: 'Initial Blog One',
+  author: 'Blog List API',
+  url: 'google.com',
+  likes: 60,
+}, {
+  title: 'Another blog in InitialBlogs',
+  author: 'Panu Valtanen',
+  url: 'youtube.com',
+  likes: 5,
+},
+];
+
+beforeEach(async () => {
+  await Blog.deleteMany({});
+  await Blog.insertMany(initialBlogs);
+});
+
+test('blogs are returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/);
+});
+
+test('correct number of blogs are returned', async () => {
+  const response = await api.get('/api/blogs');
+
+  expect(response.body).toHaveLength(initialBlogs.length);
+});
+
+afterAll(() => {
+  mongoose.connection.close();
+});
