7af42d40bb78c04162791b87ce4593ff2c313dd9
Panu Valtanen <p4nu@users.noreply.github.com>
2020-09-22 10:28:44 +0300

    4.9: blogilistan testit, step2 tehty

--- tests/blog_list_api.test.js
+++ tests/blog_list_api.test.js
@@ -36,6 +36,12 @@ test('correct number of blogs are returned', async () => {
   expect(response.body).toHaveLength(initialBlogs.length);
 });
 
+test('blog id is defined', async () => {
+  const response = await api.get('/api/blogs');
+
+  expect(response.body[0].id).toBeDefined();
+});
+
 afterAll(() => {
   mongoose.connection.close();
 });
