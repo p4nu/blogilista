1de04f5a4ba793f19b6b72ce324234454b0f7057
Panu Valtanen <p4nu@users.noreply.github.com>
2020-09-22 19:34:59 +0300

    4.14: blogilistan laajennus, step2 refaktoroitu

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -22,15 +22,7 @@ blogsRouter.post('/', async (request, response) => {
 });
 
 blogsRouter.put('/:id', async (request, response) => {
-  const body = request.body;
-
-  const blog = {
-    title: body.title,
-    author: body.author,
-    url: body.url,
-    likes: body.likes,
-  };
-
+  const blog = request.body;
   const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true });
 
   response.json(updatedBlog);
