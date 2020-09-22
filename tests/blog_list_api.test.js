d439486293a4a8ba96fa4ef1774067dde1a28b87
Panu Valtanen <p4nu@users.noreply.github.com>
2020-09-22 13:35:52 +0300

    4.13: blogilistan laajennus, step1: refaktoroitu testit käyttämään describe-lohkoja. initialBlogs siirretty test_helper.js tiedostoon.

--- tests/blog_list_api.test.js
+++ tests/blog_list_api.test.js
@@ -1,106 +1,98 @@
 const mongoose = require('mongoose');
 const supertest = require('supertest');
+const helper = require('./test_helper');
 const app = require('../app');
 const api = supertest(app);
 
 const Blog = require('../models/blog');
 
-const initialBlogs = [{
-  title: 'Initial Blog One',
-  author: 'Blog List API',
-  url: 'google.com',
-  likes: 60,
-}, {
-  title: 'Another blog in InitialBlogs',
-  author: 'Panu Valtanen',
-  url: 'youtube.com',
-  likes: 5,
-},
-];
+describe('when there are initially some blogs saved', () => {
+  beforeEach(async () => {
+    await Blog.deleteMany({});
+    await Blog.insertMany(helper.initialBlogs);
+  });
 
-beforeEach(async () => {
-  await Blog.deleteMany({});
-  await Blog.insertMany(initialBlogs);
-});
+  test('blogs are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/);
+  });
 
-test('blogs are returned as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/);
-});
+  test('correct number of blogs are returned', async () => {
+    const response = await api.get('/api/blogs');
 
-test('correct number of blogs are returned', async () => {
-  const response = await api.get('/api/blogs');
+    expect(response.body).toHaveLength(helper.initialBlogs.length);
+  });
 
-  expect(response.body).toHaveLength(initialBlogs.length);
-});
+  test('blog id is defined', async () => {
+    const response = await api.get('/api/blogs');
 
-test('blog id is defined', async () => {
-  const response = await api.get('/api/blogs');
+    expect(response.body[0].id).toBeDefined();
+  });
 
-  expect(response.body[0].id).toBeDefined();
-});
+  describe('creation of a new blog', () => {
+    test('succeeds with valid data', async () => {
+      const newBlog = {
+        title: 'This is a new blog from test',
+        author: 'Blog-list api test',
+        url: 'google.com',
+        likes: 200,
+      };
 
-test('new blog can be added', async () => {
-  const newBlog = {
-    title: 'This is a new blog from test',
-    author: 'Blog-list api test',
-    url: 'google.com',
-    likes: 200,
-  };
+      await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/);
 
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/);
+      const response = await api.get('/api/blogs');
 
-  const response = await api.get('/api/blogs');
+      const titles = response.body.map(r => r.title);
 
-  const titles = response.body.map(r => r.title);
+      expect(response.body).toHaveLength(helper.initialBlogs.length + 1);
+      expect(titles).toContain(
+        'This is a new blog from test'
+      );
+    });
 
-  expect(response.body).toHaveLength(initialBlogs.length + 1);
-  expect(titles).toContain(
-    'This is a new blog from test'
-  );
-});
+    test('succeeds to insert zero likes when it is not declared before', async () => {
+      const newBlog = {
+        title: 'This blog gets initialized with zero likes automatically',
+        author: 'Blog-list api test',
+        url: 'google.com',
+      };
 
-test('a new blog with no likes added initializes as zero likes', async () => {
-  const newBlog = {
-    title: 'This blog gets initialized with zero likes automatically',
-    author: 'Blog-list api test',
-    url: 'google.com',
-  };
+      await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/);
 
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/);
+      const response = await api.get('/api/blogs');
+      const foundBlog = response.body.find(blog =>
+        blog.title === 'This blog gets initialized with zero likes automatically'
+      );
 
-  const response = await api.get('/api/blogs');
-  const foundBlog = response.body.find(blog =>
-    blog.title === 'This blog gets initialized with zero likes automatically'
-  );
+      expect(response.body).toHaveLength(helper.initialBlogs.length + 1);
+      expect(foundBlog.likes).toEqual(0);
+    });
 
-  expect(response.body).toHaveLength(initialBlogs.length + 1);
-  expect(foundBlog.likes).toEqual(0);
-});
+    test('fails with status code 400 if title and url are not defined', async () => {
+      const newBlog = {
+        author: 'AP'
+      };
 
-test('blog without title and url returns status code 400', async () => {
-  const newBlog = {
-    author: 'AP'
-  };
+      await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(400);
 
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(400);
+      const response = await api.get('/api/blogs');
 
-  const response = await api.get('/api/blogs');
-
-  expect(response.body).toHaveLength(initialBlogs.length);
+      expect(response.body).toHaveLength(helper.initialBlogs.length);
+    });
+  });
 });
 
 afterAll(() => {
---
+++ tests/test_helper.js
@@ -0,0 +1,15 @@
+const initialBlogs = [{
+  title: 'Initial Blog One',
+  author: 'Blog List API',
+  url: 'google.com',
+  likes: 60,
+}, {
+  title: 'Another blog in InitialBlogs',
+  author: 'Panu Valtanen',
+  url: 'youtube.com',
+  likes: 5,
+}];
+
+module.exports = {
+  initialBlogs,
+};
