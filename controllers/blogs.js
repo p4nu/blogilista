75ae785c50fc3767625a6363118147d91680e763
Panu Valtanen <p4nu@users.noreply.github.com>
2020-09-28 15:01:50 +0300

    4.17: blogilistan laajennus, step5 tehty

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,8 +1,11 @@
 const blogsRouter = require('express').Router();
 const Blog = require('../models/blog');
+const User = require('../models/user');
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({});
+  const blogs = await Blog
+    .find({})
+    .populate('user', { username: 1, name: 1, id: 1 });
 
   response.json(blogs);
 });
@@ -10,13 +13,19 @@ blogsRouter.get('/', async (request, response) => {
 blogsRouter.post('/', async (request, response) => {
   const body = request.body;
 
+  const user = await User.findById(body.userId);
+
   const blog = new Blog({
     title: body.title,
     author: body.author,
     url: body.url,
     likes: body.likes || 0,
+    user: user._id,
   });
+
   const result = await blog.save();
+  user.blogs = user.blogs.concat(result._id);
+  await user.save();
 
   response.status(201).json(result);
 });
