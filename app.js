727e6bc1f42f1a00316eb61ad906a4ca5c0c5a9e
Panu Valtanen <p4nu@users.noreply.github.com>
2020-09-28 19:25:56 +0300

    4.18: blogilistan laajennus, step6 tehty: luotu route ja request kirjautumiselle

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express();
 const mongoose = require('mongoose');
 const cors = require('cors');
 const config = require('./utils/config');
+const loginRouter = require('./controllers/login');
 const usersRouter = require('./controllers/users');
 const blogsRouter = require('./controllers/blogs');
 const middleWare = require('./utils/middleware');
@@ -19,6 +20,7 @@ mongoose.connect(config.MONGODB_URI, {
 app.use(cors());
 app.use(express.json());
 
+app.use('/api/login', loginRouter);
 app.use('/api/users', usersRouter);
 app.use('/api/blogs', blogsRouter);
 
