9f4cf9ce5fcc7553cf032eb61457859bc6f688db
Panu Valtanen <p4nu@users.noreply.github.com>
2020-09-29 12:17:55 +0300

    4.21: blogilistan laajennus, step9 tehty. luotu delete_blog.rest request ja muokattu blogin poisto vaatimaan tokenin

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -44,7 +44,23 @@ blogsRouter.put('/:id', async (request, response) => {
 });
 
 blogsRouter.delete('/:id', async (request, response) => {
-  await Blog.findByIdAndRemove(request.params.id);
+  const blog = await Blog.findById(request.params.id);
+
+  const decodedToken = jwt.verify(request.token, process.env.SECRET);
+
+  if (!request.token || !decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' });
+  }
+
+  const user = await User.findById(decodedToken.id);
+
+  if (blog.user.toString() !== user._id.toString()) {
+    return response.status(401).json({
+      error: 'removing other peoples blogs is not allowed'
+    }).end();
+  }
+
+  await Blog.deleteOne(blog);
   response.status(204).end();
 });
 
