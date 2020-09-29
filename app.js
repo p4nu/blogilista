3d95abb8883d228811db14009ee817aeb035853a
Panu Valtanen <p4nu@users.noreply.github.com>
2020-09-29 11:42:57 +0300

    4.20: blogilistan laajennus, step8 tehty. refaktoroitu tokenin erottaminen middlewareksi

--- app.js
+++ app.js
@@ -21,6 +21,8 @@ mongoose.connect(config.MONGODB_URI, {
 app.use(cors());
 app.use(express.json());
 
+app.use(middleWare.tokenExtractor);
+
 app.use('/api/login', loginRouter);
 app.use('/api/users', usersRouter);
 app.use('/api/blogs', blogsRouter);
--- utils/middleware.js
+++ utils/middleware.js
@@ -14,6 +14,22 @@ const errorHandler = (error, request, response, next) => {
   next(error);
 };
 
+const tokenExtractor = (request, response, next) => {
+  const getTokenFrom = request => {
+    const authorization = request.get('authorization');
+
+    if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+      return authorization.substring(7);
+    }
+    return null;
+  };
+
+  request.token = getTokenFrom(request);
+
+  next();
+};
+
 module.exports = {
-  errorHandler
+  errorHandler,
+  tokenExtractor,
 };
