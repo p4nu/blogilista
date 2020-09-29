9e2b36a53159d17c997e25f79daa4b1f091500de
Panu Valtanen <p4nu@users.noreply.github.com>
2020-09-29 10:21:40 +0300

    4.19: blogilistan laajennus, step7: lisätty vaatimus kirjautuneesta käyttäjästä blogia luodessa

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,19 +1,26 @@
+const jwt = require('jsonwebtoken');
 const blogsRouter = require('express').Router();
 const Blog = require('../models/blog');
 const User = require('../models/user');
 
-blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog
-    .find({})
-    .populate('user', { username: 1, name: 1, id: 1 });
+const getTokenFrom = request => {
+  const authorization = request.get('authorization');
 
-  response.json(blogs);
-});
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7);
+  }
+  return null;
+};
 
 blogsRouter.post('/', async (request, response) => {
   const body = request.body;
+  const token = getTokenFrom(request);
+  const decodedToken = jwt.verify(token, process.env.SECRET);
 
-  const user = await User.findById(body.userId);
+  if (!token || !decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' });
+  }
+  const user = await User.findById(decodedToken.id);
 
   const blog = new Blog({
     title: body.title,
@@ -28,6 +35,14 @@ blogsRouter.post('/', async (request, response) => {
   await user.save();
 
   response.status(201).json(result);
+});
+
+blogsRouter.get('/', async (request, response) => {
+  const blogs = await Blog
+    .find({})
+    .populate('user', { username: 1, name: 1, id: 1 });
+
+  response.json(blogs);
 });
 
 blogsRouter.put('/:id', async (request, response) => {
